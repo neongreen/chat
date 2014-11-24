4615d4b3647ac4e832c1a0654b0d2b8d89bc7b34
eiqo <eiqo@users.noreply.github.com>
2014-11-24 00:34:12 +0000

    added nicks (unfinished)

--- Chat.hs
+++ Chat.hs
@@ -1,16 +1,20 @@
 module Chat where
 
-import Control.Concurrent
-import Control.Concurrent.Chan
-import Control.Monad
-import Control.Monad.Fix (fix)
-import Data.Time.Format
-import Data.Time.LocalTime
-import Network.Socket
-import System.IO
-import System.Locale (defaultTimeLocale)
+import           Control.Concurrent
+import           Control.Concurrent.Chan
+import           Control.Monad
+import           Control.Monad.Fix (fix)
+import           Data.IORef
+import           Data.List (isPrefixOf)
+import qualified Data.Map as M
+import           Data.Time.Format
+import           Data.Time.LocalTime
+import           Network.Socket
+import           System.IO
+import           System.Locale (defaultTimeLocale)
 
-type Msg = (Int,ZonedTime,String)
+type Msg = (Int,ZonedTime,String,String)
+type Users = IORef (M.Map Int String)
 
 main :: IO ()
 main = do
@@ -23,29 +27,38 @@ main = do
     -- allow a maximum of 2 outstanding connections
     listen sock 2
     chan <- newChan
-    mainLoop sock chan 0
+    nicksRef <- newIORef M.empty
+    mainLoop sock chan 0 nicksRef
 
-mainLoop :: Socket -> Chan Msg -> Int -> IO ()
-mainLoop sock chan id = do
+mainLoop :: Socket -> Chan Msg -> Int -> Users -> IO ()
+mainLoop sock chan id nref = do
     conn <- accept sock
-    forkIO (runConn conn chan id)
+    forkIO (runConn conn chan id nref)
     putStrLn "connection attempt"
-    mainLoop sock chan (id+1)
+    mainLoop sock chan (id+1) nref
 
-runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
-runConn (sock, _) chan id = do
-    let broadcast msg = do
+runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> Users -> IO ()
+runConn (sock, _) chan id nref = do
+    let broadcast nick msg = do
           t <- getZonedTime
           let time = formatTime defaultTimeLocale "%T" t
-              line = "["++time++"] " ++ "["++show id++"] " ++ msg
-          writeChan chan (id,t,line)
+              line = "["++time++"] " ++ nick ++ ": " ++ msg
+          writeChan chan (id,t,nick,line)
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl NoBuffering
-    broadcast ("[user " ++ show id ++ " connected]\n")
+    hPutStr hdl "enter a nickname: "
+    nick <- fix $ \loop -> do
+        nicks <- readIORef nref
+        line <- liftM init (hGetLine hdl)
+        let isTaken = line `elem` M.elems nicks
+        if isTaken
+          then hPutStr hdl "taken! try again: " >> loop
+          else modifyIORef' nref (M.insert id line) >> return line
+    broadcast nick ("[user " ++ show id ++ " connected]")
     chan' <- dupChan chan
     -- fork off thread for reading from the duplicated channel
     forkIO $ fix $ \loop -> do
-        (i,t,line) <- readChan chan'
+        (i,t,n,line) <- readChan chan'
         if i /= id
            then hPutStrLn hdl line
            else return ()
@@ -53,5 +66,5 @@ runConn (sock, _) chan id = do
     -- read lines from socket and echo them back to the user
     fix $ \loop -> do
         line <- liftM init (hGetLine hdl)
-        broadcast line
+        broadcast nick line
         loop
